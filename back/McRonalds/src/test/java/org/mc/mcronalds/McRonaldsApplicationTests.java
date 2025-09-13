7ff2015aed6c237988eec90c61a12096bbcd06ac
yagideus <angel_jas5020@hotmail.com>
2025-09-13 00:11:37 -0500

    estructura inicial del proyecto - back y front

---
+++ back/McRonalds/src/test/java/org/mc/mcronalds/McRonaldsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mc.mcronalds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McRonaldsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
